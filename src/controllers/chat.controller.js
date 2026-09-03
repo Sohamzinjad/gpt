28f305fe3e0bb0a80e045c7d007648b0bfcf8a30
Soham Zinjad <sohamzinjad@gmail.com>
2026-09-03 15:02:08 +0530

    feat: implement chat creation controller and update chat schema with user reference and timestamps

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,27 @@
+import chatModel from "../DB/Models/chat.model";
+import userModel from "../DB/Models/user.model";
+
+async function createChat(req, res) {
+    const {title} = req.body
+    const {user} = req.body
+
+    const userExists = await userModel.findById(user)
+    if(!userExists){
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+
+    const chat = await chatModel.create({
+        user,
+        title: "New Chat"
+    })
+
+    return res.status(201).json({
+        message: "Chat created successfully",
+        chat
+    })
+
+}
+
+module.exports = createChat
--- src/routes/chats.routes.js
+++ src/routes/chats.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import authUser  from "../middleware.js/auth.middleware.js"
+import chatController from "../controllers/chat.controller.js"
 
 const router = express.Router()
 
 
 
-router.post("/" , authMiddleware.authUser)
+router.post("/" , authMiddleware.authUser , chatController.createChat.createChat)
 
 
 module.export = router  
